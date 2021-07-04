f5951bb3c59f3a4e8ca7f06c62c9298ff33e9bc1
ddmrc <63403841+ddmrc@users.noreply.github.com>
2021-07-04 23:09:29 +0200

    Fixed naming folder AIComponents

--- Source/StealthGame/Private/AIComponents/Tasks/MyBTTask_MoveToLocation.cpp
+++ Source/StealthGame/Private/AIComponents/Tasks/MyBTTask_MoveToLocation.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AICompontents/Tasks/MyBTTask_MoveToLocation.h"
+#include "AIComponents/Tasks/MyBTTask_MoveToLocation.h"
 #include "Kismet/GameplayStatics.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
